b4881df96530eeb40c25e7000a045742de420873
Generalblaq1 <kingsleyadu5@gmail.com>
2022-12-19 16:49:54 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 
 /**
  *
- *  * reset_to_98 - a function that takes a pointer to an int as parameter
- *  * and updates the value it points to to 98
- *  * @n: inp
- *  * Return: n
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: inp
+ * Return: n
  */
 
 void reset_to_98(int *n)
